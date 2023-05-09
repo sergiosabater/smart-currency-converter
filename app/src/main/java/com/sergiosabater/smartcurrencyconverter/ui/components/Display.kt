69974346eb4b179b839f2a4ef02fb6b5d3adabfb
Sergio Sabater <sergiosabater@gmail.com>
2023-05-09 10:27:59 +0200

    Fix: Fixed display problem with long numbers

--- app/src/main/java/com/sergiosabater/smartcurrencyconverter/ui/components/Display.kt
+++ app/src/main/java/com/sergiosabater/smartcurrencyconverter/ui/components/Display.kt
@@ -1,15 +1,16 @@
 package com.sergiosabater.smartcurrencyconverter.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,13 +25,22 @@ class Display {
                 .background(Color.White)
                 .padding(20.dp)
         ) {
-            Text(
-                text = displayText,
-                fontWeight = FontWeight.Light,
-                fontSize = 60.sp,
-                color = Color.Black,
-                modifier = Modifier.align(Alignment.CenterEnd)
-            )
+            //Desplazamiento en horizontal del display
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(rememberScrollState())
+                    .align(Alignment.CenterEnd)
+            ) {
+                //Formato de fuente del display
+                Text(
+                    text = displayText,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 60.sp,
+                    color = Color.Black,
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                )
+            }
         }
     }
 }
