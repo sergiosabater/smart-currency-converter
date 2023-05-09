9a137022e246320c06967bc690935987b772c856
Sergio Sabater <sergiosabater@gmail.com>
2023-05-09 20:06:23 +0200

    Improved dropdown performance

--- app/src/main/java/com/sergiosabater/smartcurrencyconverter/ui/components/CurrencyDropdown.kt
+++ app/src/main/java/com/sergiosabater/smartcurrencyconverter/ui/components/CurrencyDropdown.kt
@@ -1,17 +1,19 @@
 package com.sergiosabater.smartcurrencyconverter.ui.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.*
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,22 +22,37 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowDropDown
-import androidx.compose.material3.MaterialTheme
+
 
 class CurrencyDropdown {
 
-    @OptIn(ExperimentalMaterial3Api::class)
+    val dropdownTextStyle = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        color = Color.Black
+    )
+
     @Composable
     fun CustomDropdown() {
-        val countries = listOf("Estados Unidos", "Canadá", "México", "España", "Francia")
+        val countries = remember {
+            listOf(
+                "Estados Unidos", "Canadá", "México", "España", "Francia",
+                "Italia", "Alemania", "Portugal", "Japón", "Australia",
+                "Reino Unido", "Irlanda", "Suecia", "Noruega", "Dinamarca",
+                "Suiza", "Bélgica", "Países Bajos", "Austria", "Grecia",
+                "Finlandia", "Rusia", "China", "India", "Brasil",
+                "Argentina", "Colombia", "Chile", "Uruguay", "Perú",
+                "Venezuela", "Costa Rica", "Panamá", "Puerto Rico", "República Dominicana",
+                "Cuba", "Honduras", "El Salvador", "Nicaragua", "Guatemala"
+            )
+        }
 
-        var isExpanded1 by remember { mutableStateOf(false) }
-        var isExpanded2 by remember { mutableStateOf(false) }
+        val (isExpanded1, onExpandedChange1) = remember { mutableStateOf(false) }
+        val (selectedCountry1, onSelectedCountryChange1) = remember { mutableStateOf(countries[0]) }
 
-        var selectedCountry1 by remember { mutableStateOf(countries[0]) }
-        var selectedCountry2 by remember { mutableStateOf(countries[0]) }
+        val (isExpanded2, onExpandedChange2) = remember { mutableStateOf(false) }
+        val (selectedCountry2, onSelectedCountryChange2) = remember { mutableStateOf(countries[0]) }
+
 
         Row(
             modifier = Modifier
@@ -46,14 +63,14 @@ class CurrencyDropdown {
             CustomExposedDropdown(
                 countries = countries,
                 isExpanded = isExpanded1,
-                onExpandedChange = { isExpanded1 = it },
+                onExpandedChange = onExpandedChange1,
                 selectedCountry = selectedCountry1,
-                onSelectedCountryChange = { selectedCountry1 = it },
-                modifier = Modifier.weight(1f) // Aplica el modificador weight para dividir el ancho disponible
+                onSelectedCountryChange = onSelectedCountryChange1,
+                modifier = Modifier.weight(1f)
             )
 
             Text(
-                text = "➔", // Flecha entre los dropdowns
+                text = "➔",
                 fontSize = 25.sp,
                 fontWeight = FontWeight.Light,
                 modifier = Modifier
@@ -64,15 +81,14 @@ class CurrencyDropdown {
             CustomExposedDropdown(
                 countries = countries,
                 isExpanded = isExpanded2,
-                onExpandedChange = { isExpanded2 = it },
+                onExpandedChange = onExpandedChange2,
                 selectedCountry = selectedCountry2,
-                onSelectedCountryChange = { selectedCountry2 = it },
-                modifier = Modifier.weight(1f) // Aplica el modificador weight para dividir el ancho disponible
+                onSelectedCountryChange = onSelectedCountryChange2,
+                modifier = Modifier.weight(1f)
             )
         }
     }
 
-    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     private fun CustomExposedDropdown(
         countries: List<String>,
@@ -80,47 +96,40 @@ class CurrencyDropdown {
         onExpandedChange: (Boolean) -> Unit,
         selectedCountry: String,
         onSelectedCountryChange: (String) -> Unit,
-        modifier: Modifier = Modifier // Agrega un parámetro de modificador
+        modifier: Modifier = Modifier
     ) {
-        ExposedDropdownMenuBox(
-            expanded = isExpanded,
-            onExpandedChange = onExpandedChange,
-            modifier = modifier // Usa el modificador pasado como parámetro
-        ) {
-            TextField(
-                value = selectedCountry,
-                onValueChange = {},
-                readOnly = true,
-                trailingIcon = { // Icono vacío para ocultar la flecha hacia abajo
-                    Icon(
-                        imageVector = Icons.Outlined.ArrowDropDown,
-                        contentDescription = null,
-                        tint = Color.Transparent
-                    ) },
-                colors = ExposedDropdownMenuDefaults.textFieldColors(
-                    textColor = Color.Black, // Cambia el color del texto a negro
-                    cursorColor = MaterialTheme.colorScheme.onSurface,
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    containerColor = Color.Transparent
-                ),
-                modifier = Modifier.menuAnchor(),
-                maxLines = 1, // Establece el número máximo de líneas en 1
-                textStyle = TextStyle(fontSize = 14.sp) // Cambia el tamaño de la fuente a 14.sp
-            )
+        Box(modifier.wrapContentHeight()) {
+            Box(
+                modifier = modifier.clickable(onClick = { onExpandedChange(true) })
+            ) {
+                Text(
+                    text = selectedCountry,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier
+                        .padding(start = 16.dp, end = 8.dp)
+                        .align(Alignment.CenterStart)
+                )
+            }
 
-            ExposedDropdownMenu(
+            DropdownMenu(
                 expanded = isExpanded,
                 onDismissRequest = { onExpandedChange(false) }
             ) {
                 countries.forEach { country ->
-                    DropdownMenuItem(
-                        text = { Text(text = country) },
-                        onClick = {
-                            onSelectedCountryChange(country)
-                            onExpandedChange(false)
-                        }
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                onSelectedCountryChange(country)
+                                onExpandedChange(false)
+                            }
+                    ) {
+                        Text(
+                            text = country,
+                            style = dropdownTextStyle,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
                 }
             }
         }
